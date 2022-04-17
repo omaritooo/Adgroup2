3e582f3c1a1aadf84a5121093121bd5297b0a06c
omaritooo <themedic227@gmail.com>
2022-04-17 19:13:03 +0200

    resp

--- components/Vreal/Carousel.vue
+++ components/Vreal/Carousel.vue
@@ -1,36 +1,21 @@
 <template>
     <div class="bg-[#F7F6F6]">
         <div
-            class="font-[700] uppercase mb-[52px] text-center text-[24px] leading-[36px] text-transparent bg-clip-text bg-gradient-to-l from-[#0CA092] to-[#74C38B]"
-        >OuR Services</div>
+            class="font-[700] uppercase mb-[52px] text-center text-[24px] leading-[36px] text-transparent bg-clip-text bg-gradient-to-l from-[#0CA092] to-[#74C38B]">
+            OuR Services</div>
         <client-only>
             <div>
                 <swiper class="h-full overflow-x-hidden swipe" :options="swiperOption">
-                    <swiper-slide
-                        v-for="inf in services"
-                        :key="inf.index"
-                        class="relative"
-                        @click="$router.push(`/vreal/services/${inf.id}`)"
-                    >
-                        <div
-                            class="relative z-30"
-                            @click="$router.push(`/vreal/services/${inf.id}`)"
-                        >
-                            <img
-                                :src="inf.thumbnail_path"
-                                class="w-full h-full bg-black rounded-lg"
-                                alt
-                                @click="$router.push(`/vreal/services/${inf.id}`)"
-                            />
+                    <swiper-slide v-for="inf in services" :key="inf.index" class="relative"
+                        @click="$router.push(`/vreal/services/${inf.id}`)">
+                        <div class="relative z-30" @click="$router.push(`/vreal/services/${inf.id}`)">
+                            <img :src="inf.thumbnail_path" class="w-full h-full bg-black rounded-lg" alt
+                                @click="$router.push(`/vreal/services/${inf.id}`)" />
                         </div>
-                        <div
-                            class="absolute top-0 left-0 z-30 w-full opacity-0 hover:opacity-100 transition-opacity duration-200 h-full bg-gradient-to-r from-[#0CA092] to-[#74C38B] text-white"
-                            @click="$router.push(`/vreal/services/${inf.id}`)"
-                        >
-                            <div
-                                class="absolute content-center mx-auto text-center align-middle right-1/2 top-1/2"
-                                @click="$router.push(`/vreal/services/${inf.id}`)"
-                            >{{ inf.title }}</div>
+                        <div class="absolute top-0 left-0 z-30 w-full opacity-0 hover:opacity-100 transition-opacity duration-200 h-full bg-gradient-to-r from-[#0CA092] to-[#74C38B] text-white"
+                            @click="$router.push(`/vreal/services/${inf.id}`)">
+                            <div class="absolute content-center mx-auto text-center align-middle right-1/2 top-1/2"
+                                @click="$router.push(`/vreal/services/${inf.id}`)">{{ inf.title }}</div>
                         </div>
                     </swiper-slide>
                     <!-- <swiper-slide>
@@ -53,14 +38,8 @@
         </client-only>
         <nuxt-link class="my-4" to="#why">
             <div class="px-4 mx-auto bg-gray-200 rounded-3xl w-fit py-7">
-                <svg
-                    class="w-6 h-6 animate-bounce"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="{2}"
-                >
+                <svg class="w-6 h-6 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="{2}">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
                 </svg>
             </div>
@@ -94,6 +73,8 @@ export default {
 
 <style>
 .swipe {
-    clip-path: ellipse(66% 51% at 50% 51%);
+    /* clip-path: ellipse(66% 51% at 50% 51%); */
+    clip-path: ellipse(50% 50% at 50% 50%);
+
 }
 </style>
